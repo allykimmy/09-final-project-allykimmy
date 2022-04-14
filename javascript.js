5d56beba8c2597ae99ccc549edca9978e53da1f0
allykimmy <78039634+allykimmy@users.noreply.github.com>
2022-04-14 15:34:11 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
-fetch("ttps://lyricsovh.docs.apiary.io/", {
+fetch("https://jsapi.apiary.io/apis/lyricsovh?_=1649968395218", {
   "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,ko;q=0.8",
     "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
   },
   "referrer": "https://lyricsovh.docs.apiary.io/",
   "referrerPolicy": "origin",
