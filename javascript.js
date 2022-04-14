048831d51e130862edf61d7e967e4821d815c3da
allykimmy <78039634+allykimmy@users.noreply.github.com>
2022-04-14 16:44:06 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,18 @@
-fetch("https://lyricsovh.docs.apiary.io/", {
+fetch("https://api.kanye.rest/", {
   "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,ko;q=0.8",
-    "cache-control": "max-age=0",
-    "if-none-match": "W/\"1606-jxUsVN3NFeAU7PvdpRgseUi2B2A\"",
     "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "cookie": "connect.sid=s%3Au2pGCG3KYXaciJi5qoaD8TVMOBOBVM_y.OB89sRkjZWtHoBnvGESsKlydInUfmr1i0aMpKScDBVQ; globalPreferences={%22privateConsole%22:false}"
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
   },
-  "referrerPolicy": "origin",
+  "referrer": "https://kanye.rest/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
   "body": null,
-  "method": "GET"
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
 });
