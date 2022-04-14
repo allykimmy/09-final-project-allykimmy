a154bfd15173c4c1c4f26294fc800d4cdae8e5b5
allykimmy <78039634+allykimmy@users.noreply.github.com>
2022-04-14 16:15:42 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
-fetch("https://jsapi.apiary.io/apis/lyricsovh?_=1649968395218", {
+fetch("https://lyricsovh.docs.apiary.io/", {
   "headers": {
-    "accept": "*/*",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "en-US,en;q=0.9,ko;q=0.8",
+    "cache-control": "max-age=0",
+    "if-none-match": "W/\"1606-jxUsVN3NFeAU7PvdpRgseUi2B2A\"",
     "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site"
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "cookie": "connect.sid=s%3Au2pGCG3KYXaciJi5qoaD8TVMOBOBVM_y.OB89sRkjZWtHoBnvGESsKlydInUfmr1i0aMpKScDBVQ; globalPreferences={%22privateConsole%22:false}"
   },
-  "referrer": "https://lyricsovh.docs.apiary.io/",
   "referrerPolicy": "origin",
   "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "include"
+  "method": "GET"
 });
